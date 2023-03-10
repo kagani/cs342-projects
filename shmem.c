8faf87ce2909a502e23fd7c14d8a6a76e8808f6e
kagani <m.k4g4n@gmail.com>
2023-03-10 22:20:57 +0300

    threads done

--- shmem.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <sys/mman.h>
-
-void* allocate_shmem(size_t size) {
-    int protection = PROT_READ | PROT_WRITE;
-    int visibility = MAP_SHARED | MAP_ANONYMOUS;
-    return mmap(NULL, size, protection, visibility, -1, 0);
-}
