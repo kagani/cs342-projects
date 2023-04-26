cc41967aa09d6b4e545d9391b2f0704eca0867ed
kagani <m.k4g4n@gmail.com>
2023-04-26 14:39:44 +0300

    Dummy node fix Should work but didn't test on ubuntu might segfault idk

--- p2/22002379-22003430/readyqueue.h
+++ p2/22002379-22003430/readyqueue.h
@@ -11,6 +11,7 @@ typedef struct Node
 {
     BurstItem *data;
     Node *next;
+    Node *prev;
 } Node;
 
 typedef struct Queue
@@ -22,19 +23,28 @@ typedef struct Queue
     pthread_mutex_t mutex;
 } Queue;
 
+/**
+ * @brief Enqueue a new item to the end of the queue
+ *
+ * @param q The queue to enqueue to
+ * @param b The burst item to enqueue
+ */
 void enqueue(Queue *q, BurstItem *b);
+
 /**
  * @brief Dequeue the first item in the queue
  *
- * @param q
+ * @param q The queue to dequeue from
+ * @param fq The queue to enqueue the dequeued item to
  */
-
 void dequeue(Queue *rq, Queue *fq);
+
 /**
  * @brief Dequeue the item at the given index
  *
- * @param list
- * @param idx
+ * @param list The queue to dequeue from
+ * @param fq The queue to enqueue the dequeued item to
+ * @param idx The index of the item to dequeue
  */
 void dequeue_at(Queue *list, Queue *fq, int idx);
 
@@ -44,6 +54,12 @@ void dequeue_at(Queue *list, Queue *fq, int idx);
  * @param list
  */
 void requeue(Queue *list);
+
+/**
+ * @brief Print the queue
+ *
+ * @param q
+ */
 void printQueue(Queue *q);
 
 #endif
