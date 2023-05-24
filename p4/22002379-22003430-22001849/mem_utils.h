f19a035915d24ce9f49993eac5a0e87f37631bcf
kagani <m.k4g4n@gmail.com>
2023-05-24 14:00:43 +0300

    Build and style fix

--- p4/22002379-22003430-22001849/mem_utils.h
+++ p4/22002379-22003430-22001849/mem_utils.h
@@ -1,5 +1,6 @@
 #include <fcntl.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 
 /**
@@ -12,12 +13,43 @@
  * @param pfn_begin
  * @param pfn_end
  */
-void free_fc(unsigned long pfn_begin, unsigned long pfn_end);
-void frame_info(int pfn);
+void free_fc(unsigned long pfnBegin, unsigned long pfnEnd);
+void frame_info(unsigned long pfn);
 void mem_used(int pid);
-void map_va(unsigned long PID, unsigned long VA);
-void pte(unsigned long PID, unsigned long VA);
-void map_range(unsigned long PID, unsigned long VA1, unsigned long VA1);
+
+/**
+ * @brief
+pvm -mapva PID VA: When invoked with this option, your program
+will find and print out the physical address corresponding to the virtual
+address VA for the process PID. The physical address will be printed in
+4 hexadecimal in the form 0x.... The printed value will be 16 digits long.
+The first 4 digits will be 0x0000.
+*/
+void map_va(int pid, unsigned long va);
+
+/**
+ * @brief
+pvm -pte PID VA: When invoked with this option, your program will
+find and print out detailed information for the page corresponding to the
+virtual address VA of the process PID. The information will be obtained
+from /proc/PID/pagemap file. Numbers (physical frame number or
+swap offset) must be printed in hexadecimal form.
+*/
+void pte(int pid, unsigned long va);
+
+/**
+ * @brief
+pvm -maprange PID VA1 VA2: When invoked with this option, your
+program will find and print out (page number, frame number) mappings
+for the virtual address range [VA1, VA2). For each page in the range, your
+program will print a line of information that contains the page number
+and the corresponding frame number (if any). If a page in the range is not
+a used page (i.e., is not in one of the virtual memory areas of the process),
+you will print unused instead of a frame number. If a page is a used page
+(i.e., in one of the virtual memory areas of the process), but is not in
+memory, you will print not-in-memory instead of a frame number.
+*/
+void map_range(int pid, unsigned long vaBegin, unsigned long vaEnd);
 void map_all(int pid);
 void map_all_in(int pid);
 void all_table_size(int pid);
