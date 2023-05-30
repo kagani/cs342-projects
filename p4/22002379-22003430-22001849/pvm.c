d56fc0ed5a011d356ea8c56cf24bdb320bc57c55
Kagan Ilbak <69756954+kagani@users.noreply.github.com>
2023-05-30 14:37:53 +0000

    added -h flag

--- p4/22002379-22003430-22001849/pvm.c
+++ p4/22002379-22003430-22001849/pvm.c
@@ -3,7 +3,8 @@
 #include "mem_utils.h"
 
 int main(int argc, char* argv[]) {
-    if (argc < 2) {
+    if (argc < 2 || strcmp(argv[1], "-h") == 0 ||
+        strcmp(argv[1], "--help") == 0) {
         printf(
             "The program has the following options. Use one option at a "
             "time.\n\n");
